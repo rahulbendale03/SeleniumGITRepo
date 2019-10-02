acad47f19a8ad205518d6cc01035c1a652335ac2
rahulbendale03 <rahulbendale03@gmail.com>
2019-10-02 17:01:59 +0530

    Commit02Oct

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -5,7 +5,7 @@ public class HomePage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Home Page Method");
+				System.out.println("Home Page Method");
                 System.out.println("Button found");
 		
 	}
