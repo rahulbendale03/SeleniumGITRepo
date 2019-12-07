9ac7cbd599f21dbc459c70b717ec3977d59c6539
rahulbendale03 <rahulbendale03@gmail.com>
2019-12-07 20:34:59 +0530

    New file added

--- src/main/java/com/qa/pages/ExitPage.java
+++ src/main/java/com/qa/pages/ExitPage.java
@@ -1,11 +1,11 @@
 package com.qa.pages;
 
-public class LoginPage {
+public class ExitPage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("LoginPages");
+				System.out.println("Exit Page...");
+               
 	}
-
 }
