617d7a4b3cd1d23d3c269105722a03a75bfbbe12
rahulbendale03 <rahulbendale03@gmail.com>
2020-01-29 22:13:40 +0530

    New class added

---
+++ src/main/java/com/qa/pages/CopyOfExitPage.java
@@ -0,0 +1,12 @@
+package com.qa.pages;
+
+public class CopyOfExitPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+				System.out.println("Exit Page...");
+				System.out.println("End...");
+               
+	}
+}
