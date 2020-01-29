ea9f615339d0958cc053b410b11f527f21f17281
rahulbendale03 <rahulbendale03@gmail.com>
2020-01-29 22:08:14 +0530

    dummy

---
+++ src/test/java/com/qa/test/CopyOfTestMethod1.java
@@ -0,0 +1,18 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class CopyOfTestMethod1 {
+
+	@Test
+	public void testmethod()
+	{
+		System.out.println("test method2");
+	}
+	
+	@Test
+	public void testmethod2()
+	{
+		System.out.println("test method3");
+	}
+}
