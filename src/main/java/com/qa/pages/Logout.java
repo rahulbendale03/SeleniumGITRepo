c8ab867403853637758ce567835132aec1356d75
rahulbendale03 <rahulbendale03@gmail.com>
2020-03-10 13:42:16 +0530

    LogoutPage

---
+++ src/main/java/com/qa/pages/Logout.java
@@ -0,0 +1,12 @@
+package com.qa.pages;
+
+import org.testng.annotations.Test;
+
+public class Logout {
+
+	@Test
+	public void Logout()
+	{
+		System.out.println("Logout");
+	}
+}
---
+++ src/main/java/com/qa/pages/NewProfile.java
@@ -0,0 +1,12 @@
+package com.qa.pages;
+
+import org.testng.annotations.Test;
+
+public class NewProfile {
+
+	@Test
+	public void newPerson()
+	{
+		System.out.println("New Profile");
+	}
+}
